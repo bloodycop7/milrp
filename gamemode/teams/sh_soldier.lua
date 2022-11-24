cd58870b06a500e04ede96f35ed8f47fc770141c
Mike White <111154202+MikeWhite23@users.noreply.github.com>
2022-11-24 15:12:02 +0100

    Update sh_soldier.lua

--- gamemode/teams/sh_soldier.lua
+++ gamemode/teams/sh_soldier.lua
@@ -1,5 +1,6 @@
 TEAM_SOLDIER = mrp.Teams.Define({
     name = "Soldier",
     color = Color(40, 85, 55),
-    model = "models/bread/cod/characters/milsim/shadow_company.mdl"
+    model = "models/bread/cod/characters/milsim/shadow_company.mdl",
+    description = "soldier description"
 })
